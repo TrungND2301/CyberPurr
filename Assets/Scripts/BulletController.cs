33a1db9f8495991ffcb73586c4edee7528feb6f0
Trung <trung.nguyenduc2@gameloft.com>
2021-11-10 13:46:38 +0700

    gun follow mouse position

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class BulletController : MonoBehaviour
 {
     [SerializeField] float bulletSpeed = 1f;
+
     Mouse currentMouse;
     Vector3 mousePosition;
     Vector2 direction;
@@ -20,18 +21,20 @@ public class BulletController : MonoBehaviour
 
     void Update()
     {
-        // if (isFiring)
-        // {
-        //     Vector2 position = transform.position;
-        //     position.y += bulletSpeed * Time.deltaTime;
-        //     transform.position = position;
-        // }
-
-        mousePosition = Camera.main.ScreenToWorldPoint(currentMouse.position.ReadValue());
-        direction = mousePosition - transform.position;
-        angle = Vector2.SignedAngle(Vector2.up, direction);
-        angle = Mathf.Clamp(angle, -90.0f, 90.0f);
-        transform.eulerAngles = new Vector3(0, 0, angle);
+        if (isFiring)
+        {
+            // Move the bullet
+            transform.position += transform.up * bulletSpeed * Time.deltaTime;
+        }
+        else
+        {
+            // Calculator direction and angle to mouse point
+            mousePosition = Camera.main.ScreenToWorldPoint(currentMouse.position.ReadValue());
+            direction = mousePosition - transform.position;
+            angle = Vector2.SignedAngle(Vector2.up, direction);
+            angle = Mathf.Clamp(angle, -90.0f, 90.0f);
+            transform.eulerAngles = new Vector3(0, 0, angle);
+        }
     }
 
     public void Fire()
